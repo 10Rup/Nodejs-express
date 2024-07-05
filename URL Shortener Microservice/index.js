e4edcb7598817d176f12d0951e5d0322cc2fc60d
10Rup <rupmandal.lspl@gmail.com>
2024-07-05 17:34:52 +0530

    url shortener microservice

--- URL Shortener Microservice/index.js
+++ URL Shortener Microservice/index.js
@@ -2,7 +2,7 @@ let express = require('express')
 // const { Timestamp } = require('mongodb')
 let app = express()
 let env = require('dotenv').config()
-
+let dns = require('dns')
 app.use('/public', express.static(__dirname+"/public"))
 
 
@@ -10,22 +10,16 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname+"/views/index.html")
 })
 
-app.get('/api/whoami', function (req, res) {
-    // console.log(req.headers)
+app.post('/api/shorturl', (req, res)=>{
+    console.log(req)
     res.json({
-        "ipaddress": req.headers.host,
-        "language": req.headers['accept-language'],
-        "software": req.headers['user-agent']
-    });
-});
-
-app.get('/api/hello', function (req, res) {
-    res.json({ greeting: 'hello API' });
-});
-
-app.all('*', (req, res)=>{
-    res.status(400).send("Not Found")
+        shorturl: "http://localhost:3000/"
+    })
 })
+
+// app.all('*', (req, res)=>{
+//     res.status(400).send("Not Found")
+// })
 
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running on port ' + process.env.PORT)
