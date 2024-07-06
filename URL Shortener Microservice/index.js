bdeb64d1ab60db57b11bda48b92538c1843a1b0d
Rup10 <104366033+10Rup@users.noreply.github.com>
2024-07-06 21:49:01 +0530

    url shortening update

--- URL Shortener Microservice/index.js
+++ URL Shortener Microservice/index.js
@@ -3,17 +3,38 @@ let express = require('express')
 let app = express()
 let env = require('dotenv').config()
 let dns = require('dns')
+let bodyparser = require('body-parser')
 app.use('/public', express.static(__dirname+"/public"))
-
+app.use(bodyparser.urlencoded({extended:false}))
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname+"/views/index.html")
 })
 
+// app.get('/api/shorturl', (req, res)=>{
+//     console.log(req.query)
+//     res.json({
+//         shorturl: "http://localhost:3000/"
+//     })
+// })
+
+
 app.post('/api/shorturl', (req, res)=>{
-    console.log(req)
+    
+    const url = req.body.myurl
+    dns.lookup(url, (err, address)=>{
+        console.log(`${address}`)
+    })
+    const options ={
+        all: true
+    }
+    dns.lookup(url, (err, address, family) => 
+        console.log('address: %j family: IPv%s', address, family));
+    dns.lookup('geeksforgeeks.org', options, (err, addresses) => 
+        console.log('addresses: %j', addresses)); 
     res.json({
-        shorturl: "http://localhost:3000/"
+        "original_url": url,
+        "short_url": url
     })
 })
 
