09bbfbee61802cecaea1d9c78a22eb129d9b587e
10Rup <rupmandal.lspl@gmail.com>
2024-07-10 16:36:36 +0530

    url shortener

--- URL Shortener Microservice/index.js
+++ URL Shortener Microservice/index.js
@@ -4,6 +4,9 @@ let app = express()
 let env = require('dotenv').config()
 let dns = require('dns')
 let bodyparser = require('body-parser')
+const  shortid = require("shortid")
+
+
 app.use('/public', express.static(__dirname+"/public"))
 app.use(bodyparser.urlencoded({extended:false}))
 
@@ -11,36 +14,49 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname+"/views/index.html")
 })
 
-// app.get('/api/shorturl', (req, res)=>{
-//     console.log(req.query)
-//     res.json({
-//         shorturl: "http://localhost:3000/"
-//     })
-// })
+app.use('/api/shorturl', (req, res, next)=>{
+    let url =req.body.myurl.replace("https://","")
 
-
-app.post('/api/shorturl', (req, res)=>{
-    
-    const url = req.body.myurl
-    dns.lookup(url, (err, address)=>{
-        console.log(`${address}`)
-    })
     const options ={
         all: true
     }
-    dns.lookup(url, (err, address, family) => 
-        console.log('address: %j family: IPv%s', address, family));
-    dns.lookup('geeksforgeeks.org', options, (err, addresses) => 
-        console.log('addresses: %j', addresses)); 
+    let base ='http://localhost:3000'
+    let urlId = shortid.generate()
+
+    dns.lookup(url, options, (err, addressess)=>{
+        if(err){
+            console.log(`error - ${url}`)
+        }
+        else{
+           console.log(`${base}/${urlId}`)
+        }
+        
+    })
+    next();
+})
+
+app.post('/api/shorturl', (req, res)=>{
+    // console.log(req.body.myurl)
+    const url = req.body.myurl
+    // dns.lookup(url, (err, address)=>{
+    //     console.log(`${address}`)
+    // })
+    // const options ={
+    //     all: true
+    // }
+    // dns.lookup(url,options, (err, address, family) => 
+    //     console.log('address: %j family: IPv%s', address, family));
+    // dns.lookup('www.google.com',options, (err, address, family) => 
+    //     console.log('address: %j family: IPv%s', address, family));
     res.json({
         "original_url": url,
         "short_url": url
     })
 })
 
-// app.all('*', (req, res)=>{
-//     res.status(400).send("Not Found")
-// })
+app.all('*', (req, res)=>{
+    res.status(400).send("Not Found")
+})
 
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running on port ' + process.env.PORT)
