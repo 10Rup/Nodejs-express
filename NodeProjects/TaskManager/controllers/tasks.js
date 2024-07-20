f31aca4ff6cde7e9fb5bb67986301298b5eca6a9
Rup10 <104366033+10Rup@users.noreply.github.com>
2024-07-20 10:49:22 +0530

    Update tasks.js

--- NodeProjects/TaskManager/controllers/tasks.js
+++ NodeProjects/TaskManager/controllers/tasks.js
@@ -1,17 +1,33 @@
 const Task = require('../model/Task')
 
 
-const getAllTasks = (req, res)=>{
-    res.send("All Data")
+const getAllTasks = async (req, res)=>{
+    try {
+        const dataall = await Task.find({})
+        res.status(201).json({dataall})
+    } catch (error){
+        res.status(500).json({msg: error.errors.name.message})
+    }
 }
 
-const getTask = (req, res)=>{
-    res.json({id: req.params.id})
+const getTask = async (req, res)=>{
+    try {
+        const {id:filtervalue} = req.params
+        const find_data  = await Task.findOne({_id:filtervalue})
+        res.status(201).json({find_data})
+    } catch (error){
+        res.status(500).json({msg: error.errors.name.message})
+    }
 }
 
 const createTask =async (req, res)=>{
-    const newtask = await Task.create(req.body)
-    res.status(201).json({newtask})
+    try {
+        const newtask = await Task.create(req.body)
+        res.status(201).json({newtask})
+    } catch (error){
+        res.status(500).json({msg: error.errors.name.message})
+    }
+
 }
 
 const updateTask = (req, res)=>{
